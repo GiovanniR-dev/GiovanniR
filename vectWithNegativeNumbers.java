b4f0c07854ea6112b7a7232fe65fb7d4f94137c2
Giovanni Rodrigues <giovannicorrearodrigues124@gmail.com>
2026-01-30 21:14:18 -0300

    Add program to input and display negative numbers

---
+++ vectWithNegativeNumbers.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class teste1 {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Quantos numeros voce vai digitar?: ");
+        int n=sc.nextInt();
+
+        int[] vect=new int[n];
+
+        for (int i=0;i<n;i++){
+            System.out.println("Digite um numero: ");
+            vect[i]= sc.nextInt();
+        }
+
+        System.out.println("Numeros negativos");
+        for (int i=0;i<n;i++){
+            if (vect[i]<0){
+                System.out.println(vect[i]);
+            }
+        }
+
+
+
+
+
+        sc.close();
+    }
+}
