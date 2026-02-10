34beec94d8f953b26ddaeeb0d7d20e1ddc48f220
Giovanni Rodrigues <giovannicorrearodrigues124@gmail.com>
2026-02-09 21:56:40 -0300

    matriz

---
+++ matriz[i][j].java
@@ -0,0 +1,35 @@
+public class teste1 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int[][] matriz=new int[n][n];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                matriz[i][j]=sc.nextInt();
+            }
+        }
+
+        System.out.println("Main diagonal");
+        for(int i=0;i<n;i++){
+            System.out.print(matriz[i][i]+" ");
+        }
+        System.out.println();
+
+        int count=0;
+        for(int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                if (matriz[i][j]<0){
+                    count++;
+                }
+            }
+        }
+        System.out.println("Negative numbers"+count);
+
+
+        sc.close();
+    }
+
+}
